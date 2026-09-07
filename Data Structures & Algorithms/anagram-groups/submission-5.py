8164606740a7442d7e8597c21963dd1673c0e6c5
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2026-09-06 18:59:08 -0600

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getAnagram(self, strA: str) -> tuple:
+        anagramArr = []
+        for i in range(26):
+            anagramArr.append(0)
+        for i in strA:
+            index = ord(i)-97
+            anagramArr[index] = anagramArr[index] + 1
+        return tuple(anagramArr)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        outputDict = {}
+        for word in strs:
+            anagramTuple = self.getAnagram(word)
+            if anagramTuple in outputDict:
+                outputDict[anagramTuple].append(word)
+            else:
+                outputDict[anagramTuple] = [word]
+        return list(outputDict.values())
